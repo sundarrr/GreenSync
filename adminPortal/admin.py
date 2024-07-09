d208a96dcd77b70c1ce28278702e6c0ebdba3a85
Rakul Vimala Manokaran <rakulcareer6@gmail.com>
2024-07-09 12:56:28 -0400

    feature/admin_portal

--- adminPortal/admin.py
+++ adminPortal/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from mapbox_location_field.admin import MapAdmin
 
-# Register your models here.
+from .models import (
+    EventCategory,
+    Event,
+    # JobCategory,
+    # EventJobCategoryLinking,
+    EventMember,
+    EventUserWishList
+)
+
+admin.site.register(EventCategory)
+admin.site.register(Event, MapAdmin)
+admin.site.register(EventMember)
+admin.site.register(EventUserWishList)
