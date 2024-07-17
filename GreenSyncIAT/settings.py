2508337cecab58f0d35622b9cf655e2af71fe355
Saravanan Chandrasekaran <saravananc2023@gmail.com>
2024-07-17 14:04:17 -0400

    Added Trusted host

--- GreenSyncIAT/settings.py
+++ GreenSyncIAT/settings.py
@@ -29,6 +29,10 @@ SECRET_KEY = 'django-insecure-+$i1&wst0u^(77^vw9x*&fkc+pk*c+%w!fb1@hr)3k(=sr+!^o
 DEBUG = True
 
 ALLOWED_HOSTS = ["ecogreenmart.in","www.ecogreenmart.in"]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ecogreenmart.in',
+]
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
